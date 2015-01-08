e396ba9a24d8027fdca90b3b166051aded83e544
Rick Morrison <rick@do106.priologic.com>
2015-01-08 10:41:56 -0500

    fixed config.json to agree with tests

--- sensu/config_test.go
+++ sensu/config_test.go
@@ -8,11 +8,11 @@ func Test_SingleConfigFile(t *testing.T) {
 	cfg, _ := LoadConfigs("../config/config.json", []string{})
 
 	if cfg.Client.Name != "localhost" {
-		t.Errorf("expected %s, got %s", cfg.Client.Name, "localhost")
+		t.Errorf("expected %s, got %s", "localhost", cfg.Client.Name)
 	}
 
 	if cfg.Client.Address != "192.168.1.1" {
-		t.Errorf("expected %s, got %s", cfg.Client.Address, "192.168.1.1")
+		t.Errorf("expected %s, got %s", "192.168.1.1", cfg.Client.Address)
 	}
 
 	if cfg.Client.Subscriptions[0] != "test" {
